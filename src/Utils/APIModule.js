58411701b1d044094fd13076a5e107f5c11edf8d
Felix Menzel <Felix.Menzel.Dev@gmail.com>
2023-05-21 23:28:07 +0200

    done

--- src/Utils/APIModule.js
+++ src/Utils/APIModule.js
@@ -6,16 +6,29 @@ class APIModule {
 
   static get NumberOfUsers() { return 20 }
 
+
+
+  /**
+   * The SDK Method to fetch the data, it has a failure rate of 10 percent and a random delay, otherwise it will return an array of objects
+   * each object has the userName and occupation key
+   *
+   * @static
+   * @return {*}
+   * @memberof APIModule
+   */
   static async fetchData() {
-    const shouldFail = Math.random() * 100 > this.FailureRate
+    const shouldFail = Math.random() * 100 < this.FailureRate
     if (shouldFail) {
       throw new Error('something went wrong')
     }
 
-    const delay = Math.random() * 100 * 3
-    return await setTimeout(() => {
-      [...Array(APIModule.NumberOfUsers)].map(() => ({ userName: faker.internet.userName(), occupation: faker.commerce.department() }))
-    }, delay)
+    const data = [...Array(APIModule.NumberOfUsers)].map(() => ({ userName: faker.internet.userName(), occupation: faker.commerce.department() }))
+    const delay = Math.random() * 1000 * 5
+
+    /// Mocked Network delay
+    await new Promise(resolve => setTimeout(resolve, delay));
+
+    return { data }
   }
 }
 
